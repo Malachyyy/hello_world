819129d4b2ab85667f36f521e11501eab6273cf0
Huy Tran <huytran1163@gmail.com>
2025-08-12 01:51:07 +0200

    Merge conflict

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 import "fmt"
@@ -6,12 +5,3 @@ import "fmt"
 func main() {
 	fmt.Println(hello_world())
 }
-=======
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println(hello_world())
-}
->>>>>>> 7398024a5ff28e2effb2cf3679f6c4f19e34f53f
