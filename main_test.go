e62048d295c03a2680013da8e68971235a50b457
Huy Tran <huytran1163@gmail.com>
2025-08-14 23:12:15 +0200

    added log for succesful test

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestMain(t *testing.T) {
 	got := hello_world()
 	want := "Hello, World!"
+	fmt.Println("Test successful, got:", got)
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
